a483e486d06bd430d4176bcce193164296ceca90
Hasankhan170 <hasankhanali0316@gmail.com>
2024-06-27 08:17:10 -0700

    typescript

---
+++ app.ts
@@ -0,0 +1,17 @@
+// console.log('hasan');
+
+//-----Type Annotations-----//
+
+// let userName:string = 'hasan';
+// // userName = 20
+// console.log(userName);
+
+// let num:number = 20;
+// console.log(num);
+
+// let isLoggedIn:boolean = true;
+// console.log(isLoggedIn);
+
+
+
+
